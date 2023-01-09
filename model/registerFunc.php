e95fbfc8ddbe41c703e711295232344601f51c95
Judy Huang <aa09111995tw@gmail.com>
2023-01-09 19:36:06 +0900

    #5

--- model/registerFunc.php
+++ model/registerFunc.php
@@ -6,12 +6,15 @@ class registerFunction{
 
         $db = DB::connectDB();
 
-        $newEntry = $db->prepare('
-            INSERT INTO users (username, password, first_name, last_name, email, created_date)
-            VALUES (?, ?, ?, ?, ?, ?)
-        ');
-        $newEntry->execute([$entry['username'], $entry['password'], $entry['first_name'], $entry['last_name'], $entry['email'], date('Y-m-d H:i:s')]);
-    }
+        //hash password
+        $password=$entry["password"];
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
+        $newEntry = $db->prepare('
+            INSERT INTO users (username, password, first_name, last_name, email, created_date, admin)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        ');
+        $newEntry->execute([$entry['username'], $hashed_password, $entry['first_name'], $entry['last_name'], $entry['email'], date('Y-m-d H:i:s'), '0']);
+    }
 
 }
