4f0710b375524add00b6e9d72cdf67c19ec2c7e5
Judy Huang <aa09111995tw@gmail.com>
2023-01-10 20:37:07 +0900

    fix add user bug

--- model/adminFunc.php
+++ model/adminFunc.php
@@ -32,4 +32,20 @@ class adminFunction{
         $users = $db->prepare('DELETE FROM users WHERE id = ?');
         $users->execute([$id]);
     }
+
+    public function addUsers($entry){
+
+        $db = DB::connectDB();
+
+        //hash password
+        $password=$entry["password"];
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+        $newEntry = $db->prepare('
+            INSERT INTO users (username, password, first_name, last_name, email, created_date, admin)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        ');
+        $newEntry->execute([$entry['username'], $hashed_password, $entry['first_name'], $entry['last_name'], $entry['email'], date('Y-m-d H:i:s'), '0']);
+    }
+
 }
