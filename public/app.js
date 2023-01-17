c6c39125cbe71817ec8106a797afbd5b4390b14d
Judy Huang <aa09111995tw@gmail.com>
2023-01-17 19:08:41 +0900

    #18

--- public/app.js
+++ public/app.js
@@ -33,3 +33,32 @@ buttons.forEach(button => {
     });
 
 });
+
+//Google Map API show multiple locations on one map
+
+function initMap() {
+
+    // The map, centered at wcoding
+    const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: { lat: 37.53622850959400, lng: 126.894975568805080 },
+    });
+    setMarkers(map);
+}
+
+const locations = [
+    ['Sample Place 1', 37.53622850959400, 126.894975568805080],
+    ['Sample Place 2', 37.537053744792225, 126.896220113787990],
+];
+
+function setMarkers(map) {
+    for (let i = 0; i < locations.length; i++) { 
+        const location = locations[i];
+        new google.maps.Marker({
+        position: {lat: location[1], lng: location[2]},
+        map: map
+        });
+    }
+}
+
+window.initMap = initMap;
--- view/home.php
+++ view/home.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Page</title>
-</head>
-<body>
-    <h1>Welcome to On My Way</h1>
-    <h2>Please view existing places on our map</h2>
-    <h3>Please click here to login / <a href="../on-my-way/index.php?action=register">register</a></h3>
-</body>
-</html>
+<?php
+$title = 'ON MY WAY | WELCOME';
+ob_start();
+?>
+
+<h1>Welcome to On My Way</h1>
+<h2>Please view existing places on our map</h2>
+<h3>Please click here to login / <a href="../on-my-way/index.php?action=register">register</a></h3>
+<div id="map"></div>
+
+
+<?php
+    $html = ob_get_clean(); // give the code into a variable
+    include 'template.php'; // and call the variable from the template
+?>
+
 
