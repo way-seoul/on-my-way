4374df6f3c99a3362eb64f7394445f110a74f11f
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2023-02-13 22:09:50 +0900

    CSS for header & challenge specific page

--- view/header.php
+++ view/header.php
@@ -20,24 +20,26 @@
         <form method="POST" action="<?= LOGIN_PATH ?>">
             <input class="username_header" type="text" name="username_header" placeholder="Username" required>
             <input class="password_header" type="password" name="password_header" placeholder="Password" required>
-            <button name="login_button_header">LOG IN</button>
+            <button id="login_button_header" name="login_button_header">LOG IN</button>
         </form>
     <?php }else if(isset($_SESSION['user']) && !isset($_POST['log_out'])){ ?>
         <div>
-            <input type="checkbox" id="profile_toggle">
-            <label id="profile_toggle_label" for="profile_toggle"><img src="./public/image/favicon.ico" alt="profile"></label>
-            <ul id="profile_ul">
-                <li>
-                    <form method="POST" id="login-box">
-                        <button id="logout_button" type="submit" name="log_out">LOG OUT</button>
-                <?php }else if(isset($_POST['log_out'])){
-                    session_unset();
-                    session_destroy();
-                    header('Location: index.php?action=');
-                ?>
-                <?php } ?>
-                    </form>                   
-                </li>
-            </ul>
+            <div style="position: relative; margin-right: 20px;">
+                <input type="checkbox" id="profile_toggle">
+                <label id="profile_toggle_label" for="profile_toggle"><img src="./public/image/favicon.ico" alt="profile"></label>
+                <ul id="profile_ul">
+                    <li>
+                        <form method="POST" id="login-box">
+                            <button id="logout_button" type="submit" name="log_out">LOG OUT</button>
+                    <?php }else if(isset($_POST['log_out'])){
+                        session_unset();
+                        session_destroy();
+                        header('Location: index.php?action=');
+                    ?>
+                    <?php } ?>
+                        </form>                   
+                    </li>
+                </ul>
+            </div>
         </div>
 </header>
