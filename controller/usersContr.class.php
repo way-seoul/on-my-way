333a31b22534e241f691f9b55909a2265d58d9a7
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2023-02-14 19:55:07 +0900

    fixed login issues that occur when the username doesn't exsit

--- controller/usersContr.class.php
+++ controller/usersContr.class.php
@@ -14,34 +14,42 @@ Class UsersContr extends Users{
 
         if(isset($_POST['login_button']) || isset($_POST['login_button_header']) && (isset($_POST['username']) || isset($_POST['username_header'])) !== '' && (isset($_POST['password']) || isset($_POST['password_header'])) !== ''){
             $user = new Users();
+
             $username = $_POST['username'] ?? $_POST['username_header'];
             $password = $_POST['password'] ?? $_POST['password_header'];
             
             $user_info = $user->getUser($username);
-            $verified_password = password_verify($password, $user_info['password']);
-            
-            $validated = isset($username) && isset($password) && $username == $user_info['username'] && $verified_password == true ? true : false;
-            
-            if($validated == true){
-                
-                $verified_username = $user_info['username'];
-                $verified_user_id = $user_info['id'];
-                $verified_admin = $user_info['admin'];
-                
-                $_SESSION['logged_in'] = true;
-                $_SESSION['user_id'] = $verified_user_id;
-                $_SESSION['admin'] = $verified_admin;
-                $_SESSION['user'] = $verified_username;
-            }else if($validated == false){
-                echo "<script>alert('Incorrect username or password!')</script>";
-            }
-            
-            $logged_in = isset($_SESSION['logged_in']) && $_SESSION['logged_in'];
-            $is_admin = (isset($_SESSION['admin']) && $_SESSION['admin'] == 1);
 
-            if($logged_in){
-                if(!$is_admin) header('location: index.php?action=home');
-                else header('location: index.php?action=admin');
+            if($user_info == true){
+                
+                $verified_password = password_verify($password, $user_info['password']);
+                
+                $validated = isset($username) && isset($password) && $username == $user_info['username'] && $verified_password == true ? true : false;
+                
+                if($validated == true){
+                    
+                    $verified_username = $user_info['username'];
+                    $verified_user_id = $user_info['id'];
+                    $verified_admin = $user_info['admin'];
+                    
+                    $_SESSION['logged_in'] = true;
+                    $_SESSION['user_id'] = $verified_user_id;
+                    $_SESSION['admin'] = $verified_admin;
+                    $_SESSION['user'] = $verified_username;
+                }else{
+                    echo "<script>alert('Incorrect username or password!')</script>";
+                }
+                
+                $logged_in = isset($_SESSION['logged_in']) && $_SESSION['logged_in'];
+                $is_admin = (isset($_SESSION['admin']) && $_SESSION['admin'] == 1);
+    
+                if($logged_in){
+                    if(!$is_admin) header('location: index.php?action=home');
+                    else header('location: index.php?action=admin');
+                }
+                
+            }else{
+                echo "<script>alert('Incorrect username or password!')</script>";
             }
         }
         
--- model/usersManager.php
+++ model/usersManager.php
@@ -23,6 +23,7 @@ class Users extends Db {
 
         $user = $db->prepare('SELECT username, password, id, admin FROM users WHERE username = ?');
         $user->execute([$username]);
+        
         return $user->fetch();
     }
 
