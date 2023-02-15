a529bd2d581e30e68db6b3e799a0761780994f04
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2023-02-15 22:25:51 +0900

    removed a function

--- model/usersManager.php
+++ model/usersManager.php
@@ -27,30 +27,6 @@ class Users extends Db {
         return $user->fetch();
     }
 
-    public function addChallengeAchievedPoints($id, $challengePoints) {
-        $db = DB::connectDB();
-        $newTotalPoints = 0;
-        //First Get Current Points Total & Update it
-        $user = $db->prepare('SELECT points_total FROM users WHERE id = ?');
-        $user->execute([$id]);
-        $currentTotalPoints = $user->fetch();
-        if($currentTotalPoints && $currentTotalPoints > 0) {
-            $newTotalPoints = $currentTotalPoints['points_total'] + $challengePoints;
-        } else {
-            $newTotalPoints = $challengePoints;
-        }
-        //Update User Record with new points total
-        $sql = "UPDATE users
-                SET points_total=:newTotalPoints
-                WHERE id=:id";
-        $stmt= $db->prepare($sql);
-        $stmt->execute([
-            'newTotalPoints' => $newTotalPoints,
-            'id' => $id
-        ]);
-        return $newTotalPoints;
-    }
-
     public function addRecordToUserChallTable($user_id, $challenge_id) {
         $db = DB::connectDB();
         $newRecord = $db->prepare(
